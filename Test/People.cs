26f2fee8acdc67d298ba903a6c0d857d891c35de
Артем <cr1mp@ya.ru>
2016-07-15 13:00:24 +0300

    Добавил мини-игру

--- Test/People.cs
+++ Test/People.cs
@@ -6,46 +6,102 @@ using System.Threading.Tasks;
 
 namespace Test
 {
-	class Class
-	{
+    class Hero
+    {
+        public Hero( string name )
+        {
+            Name = name;
+        }
+        public string Name;
         public int Agi;
         public int Str;
         public int Int;
         public int Prot;
         public int Dmg;
+        public int HP;
+        public Weapon CurrentWeapon;
+        public Weapon[] Weapons;
+    }
 
+    class Weapon
+    {
+        public int Dmg;
+    }
 
-    }
-    class Class2
+    class Arena
     {
-        public int Agi;
-        public int Str;
-        public int Int;
-        public int Prot;
-        public int Dmg;
+        public void PvP( Hero player1, Hero player2 )
+        {
+
+            if ( player1.HP < 0 )
+            {
+                Console.WriteLine( "Победил: " + player2.Name );
+                return;
+            }
+            if ( player2.HP < 0 )
+            {
+                Console.WriteLine( "Победил: " + player1.Name );
+                return;
+            }
+
+            Battle( player1, player2 );
+
+            PvP( player1, player2 );
+        }
+
+        private void Battle( Hero player1, Hero player2 )
+        {
+            if ( player1.HP > 0 )
+                player2.HP = player2.HP - ( player1.Dmg + player1.CurrentWeapon.Dmg );
+            if ( player2.HP > 0 )
+                player1.HP -= player2.Dmg + player2.CurrentWeapon.Dmg;
+        }
+
+        public void DeathMatch( Hero[] heroes )
+        {
+            while ( AliveHeroCount( heroes ) > 1 )
+            {
+                heroes = AliveHero( heroes );
+
+                for ( int i = 0; i < heroes.Length; i++ )
+                {
+                    if ( i == heroes.Length - 1 )
+                    {
+                        Battle( heroes[0], heroes[heroes.Length - 1] );
+                    }
+                    else
+                    {
+                        Battle( heroes[i], heroes[i + 1] );
+                    }
+                }
+            }
+            heroes = AliveHero( heroes );
+            Console.WriteLine( "Победитель смертельного поединка " + heroes[0].Name );
+        }
+
+        public void TeamMatch(Hero[] red, Hero[] blue)
+        {
+        }
+
+        private int AliveHeroCount( Hero[] heroes )
+        {
+            int Count = 0;
+            foreach ( Hero hero in heroes )
+            {
+                if ( hero.HP > 0 )
+                {
+                    Count++;
+                }
+
+            }
+            return Count;
+        }
+
+        private Hero[] AliveHero( Hero[] heroes )
+        {
+            return heroes.Where( h => h.HP > 0 ).ToArray();
+        }
     }
-    class Class3
-    {
-        public int Agi;
-        public int Str;
-        public int Int;
-        public int Prot;
-        public int Dmg;
-    }
-    class Class4
-    {
-        public int Agi;
-        public int Str;
-        public int Int;
-        public int Prot;
-        public int Dmg;
-    }
-    class Class5
-    {
-        public int Agi;
-        public int Str;
-        public int Int;
-        public int Prot;
-        public int Dmg;
-    }
+
 }
+
--- Test/Program.cs
+++ Test/Program.cs
@@ -10,72 +10,109 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            var Warrior = new Class();
-            var Mage = new Class2();
-            var Assassin = new Class3();
-            var Archer = new Class4();
-            var DeathLord = new Class5();
+            var Warrior = new Hero("Dima");
+            var Mage = new Hero("Shang Tsung");
+            var Assassin = new Hero("Artem");
+            var Archer = new Hero("Arrow");
+            var DeathLord = new Hero("Darth Vader");
+
+            var Sword = new Weapon();
+            var Staff = new Weapon();
+            var Daggers = new Weapon();
+            var Bow = new Weapon();
+            var Scythe = new Weapon();
+
+            Sword.Dmg = 20;
+            Staff.Dmg = 10;
+            Daggers.Dmg = 15;
+            Bow.Dmg = 20;
+            Scythe.Dmg = 25;
+
+            Warrior.HP = 500;
             Warrior.Agi = 5;
             Warrior.Str = 9;
             Warrior.Int = 3;
             Warrior.Prot = 7;
             Warrior.Dmg = 34;
+            Warrior.CurrentWeapon = Sword;
             Console.WriteLine("Класс: Воин");
             Console.WriteLine(Warrior.Agi);
             Console.WriteLine(Warrior.Str);
             Console.WriteLine(Warrior.Int);
             Console.WriteLine(Warrior.Prot);
             Console.WriteLine(Warrior.Dmg);
-            Console.WriteLine("Оружие - Меч");
+
+            Mage.HP = 130;
             Mage.Agi = 2;
             Mage.Str = 3;
             Mage.Int = 9;
             Mage.Prot = 4;
             Mage.Dmg = 53;
+            Mage.CurrentWeapon = Staff;
             Console.WriteLine("Класс: Маг");
             Console.WriteLine(Mage.Agi);
             Console.WriteLine(Mage.Str);
             Console.WriteLine(Mage.Int);
             Console.WriteLine(Mage.Prot);
             Console.WriteLine(Mage.Dmg);
-            Console.WriteLine("Оружие - Посох");
+
+            Assassin.HP = 250;
             Assassin.Agi = 9;
             Assassin.Str = 3;
             Assassin.Int = 6;
             Assassin.Prot = 4;
             Assassin.Dmg = 67;
+            Assassin.CurrentWeapon = Daggers;
             Console.WriteLine("Класс: Ассассин");
             Console.WriteLine(Assassin.Agi);
             Console.WriteLine(Assassin.Str);
             Console.WriteLine(Assassin.Int);
             Console.WriteLine(Assassin.Prot);
             Console.WriteLine(Assassin.Dmg);
-            Console.WriteLine("Оружие - Кинжалы");
+
+            Archer.HP = 200;
             Archer.Agi = 8;
             Archer.Str = 2;
             Archer.Int = 7;
             Archer.Prot = 4;
             Archer.Dmg = 70;
+            Archer.CurrentWeapon = Bow;
             Console.WriteLine("Класс: Лучник");
             Console.WriteLine(Archer.Agi);
             Console.WriteLine(Archer.Str);
             Console.WriteLine(Archer.Int);
             Console.WriteLine(Archer.Prot);
             Console.WriteLine(Archer.Dmg);
-            Console.WriteLine("Оружие - Лук");
+
+
+            DeathLord.HP = 400;
             DeathLord.Agi = 5;
             DeathLord.Str = 7;
             DeathLord.Int = 7;
             DeathLord.Prot = 5;
             DeathLord.Dmg = 90;
+            DeathLord.CurrentWeapon = Scythe;
             Console.WriteLine("Класс: Повелитель Смерти");
             Console.WriteLine(DeathLord.Agi);
             Console.WriteLine(DeathLord.Str);
             Console.WriteLine(DeathLord.Int);
             Console.WriteLine(DeathLord.Prot);
             Console.WriteLine(DeathLord.Dmg);
-            Console.WriteLine("Оружие - Коса");
+
+            var _100XRay = new Arena();
+            _100XRay.PvP(Warrior, DeathLord);
+            
+            Hero[] heroes = new Hero[5];
+            heroes[0] = Warrior;
+            heroes[1] = Mage;
+            heroes[2] = Archer;
+            heroes[3] = Assassin;
+            heroes[4] = DeathLord;
+
+            _100XRay.DeathMatch(heroes);
+
             Console.ReadLine();
+
             
         }
       
