65c23ec371768686b6c45a45ab276444f98f8fa0
Артем <cr1mp@ya.ru>
2016-09-17 14:58:29 +0300

    Добавил бинарный поиск числа, наркоманию и алгоритм удаления лишних пробелов

---
+++ Remembers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Remembers
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("Hello, world!");
+			Console.ResetColor();
+			
+			int b = 3;
+			int c = 2;
+			for (int i = 0; i <= 10; i++)
+			{
+				if (i == 4)
+			{
+				Console.ForegroundColor = ConsoleColor.Green;
+				Console.WriteLine(i + " Да");
+			}
+			else
+			{
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine(i + " Нет");
+			}
+			}
+			
+			Console.ReadLine();
+		}
+	}
+}
